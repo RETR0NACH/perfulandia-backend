a588a437bcec335a60fb5256deed8520a3835eaa
RETR0NACH <ichaconaedo@gmail.com>
2025-05-25 13:24:17 -0400

    Code CarritoService completo, probado y funcional

--- carritoservice/src/main/java/com/perfulandia/carritoservice/repository/CarritoRepository.java
+++ carritoservice/src/main/java/com/perfulandia/carritoservice/repository/CarritoRepository.java
@@ -4,7 +4,7 @@ package com.perfulandia.carritoservice.repository;
 import com.perfulandia.carritoservice.model.Carrito;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-//
+//as
 
 @Repository
 public interface CarritoRepository extends JpaRepository<Carrito, Long>{
